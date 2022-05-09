7e6204e08435fc750c0fb9268dcfc478321166c9
MinZhi <chua.minzhi@gmail.com>
2022-05-09 16:54:38 +0200

    first commit

---
+++ NLP_API.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+import pandas as pd
+
+import numpy as no
+
+import pickle
+
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+st.write("""
+
+# Emotion Diary Prediction App
+
+
+This app predicts the emotion of a user based on their Emotion Diary. 
+
+""")
