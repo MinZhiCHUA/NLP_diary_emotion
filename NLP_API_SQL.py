1fc771a188a60173d1ca2e0cc37803712199a8d8
MinZhi <chua.minzhi@gmail.com>
2022-05-10 16:08:00 +0200

    sql update

--- NLP_API_SQL.py
+++ NLP_API_SQL.py
@@ -42,7 +42,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from user_info;")
 
 # Print results.
 for row in rows:
