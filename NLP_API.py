a06c34b27a413b2bc78f524422aa49e84665bea2
MinZhi <chua.minzhi@gmail.com>
2022-05-10 08:45:59 +0200

    add image to main page

--- NLP_API.py
+++ NLP_API.py
@@ -1,14 +1,11 @@
 import streamlit as st
-
 import pandas as pd
-
 import numpy as no
-
 import pickle
-
 import seaborn as sns
-
 import matplotlib.pyplot as plt
+
+from PIL import Image
 
 st.write("""
 
@@ -18,3 +15,7 @@ st.write("""
 This app predicts the emotion of a user based on their Emotion Diary. 
 
 """)
+
+image = Image.open("img/wordcloud.png") 
+
+st.image(image=image)
